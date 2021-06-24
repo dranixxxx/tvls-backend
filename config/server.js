5e317f40be8fe5de33022c92ba6af2bca6980c2b
dranixxxx <phoang0302@gmail.com>
2021-06-24 11:58:29 +0700

    new file:   .editorconfig 	new file:   .env.example 	new file:   .eslintignore 	new file:   .eslintrc 	new file:   .gitignore 	new file:   README.md 	new file:   api/.gitkeep 	new file:   api/cung/config/routes.json 	new file:   api/cung/controllers/cung.js 	new file:   api/cung/models/cung.js 	new file:   api/cung/models/cung.settings.json 	new file:   api/cung/services/cung.js 	new file:   api/loigiai/config/routes.json 	new file:   api/loigiai/controllers/loigiai.js 	new file:   api/loigiai/models/loigiai.js 	new file:   api/loigiai/models/loigiai.settings.json 	new file:   api/loigiai/services/loigiai.js 	new file:   api/lucthan/config/routes.json 	new file:   api/lucthan/controllers/lucthan.js 	new file:   api/lucthan/models/lucthan.js 	new file:   api/lucthan/models/lucthan.settings.json 	new file:   api/lucthan/services/lucthan.js 	new file:   api/sao/config/routes.json 	new file:   api/sao/controllers/sao.js 	new file:   api/sao/models/sao.js 	new file:   api/sao/models/sao.settings.json 	new file:   api/sao/services/sao.js 	new file:   config/database.js 	new file:   config/functions/bootstrap.js 	new file:   config/functions/cron.js 	new file:   config/functions/responses/404.js 	new file:   config/server.js 	new file:   extensions/.gitkeep 	new file:   extensions/users-permissions/config/jwt.js 	new file:   favicon.ico 	new file:   package.json 	new file:   public/robots.txt 	new file:   public/uploads/.gitkeep 	new file:   yarn.lock

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2415a9a1bfce577e4273faaddda39aa5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4512b759-63f1-4e4c-8bd2-bf97ed8f71c1'
+};
